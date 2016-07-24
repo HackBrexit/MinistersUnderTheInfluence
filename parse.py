d8d5578d3e9f9202508da6b2001df9fb733f43ef
Elianne Schutze <eliannemsr@gmail.com>
2016-07-24 13:13:23 +0100

    before presentation

--- parse.py
+++ parse.py
@@ -8,14 +8,15 @@ def main():
     if(len(sys.argv)>1):
         inputfile = sys.argv[1]
 
+    startWithNote = re.compile("^Note")
     year = getYearFromFilename(inputfile)
 
-    startWithNote = re.compile("^Note")
-
     with open(inputfile,'r') as file:
+
         csvReader = csv.reader(file,delimiter=',',quotechar='\"')
 
         minister = "";
+        next(csvReader,None)
 
         for line in csvReader:
             if(startWithNote.match(line[0])):
@@ -25,21 +26,37 @@ def main():
                 minister = line[0]
 
             if(not isBlankLine(line)):
-                writeToFile(line,minister)
+                writeToFile(line,minister,year,inputfile)
 
 
 def isBlankLine(line):
     return(len(line[1])==0 and len(line[2])==0)
 
-def writeToFile(line,minister):
-    writeMinisters(minister)
+def writeToFile(cols,minister,year,inputfile):
+    with open("combinedmeetings.csv",'a') as newFile:
+        csvWriter = csv.writer(newFile, delimiter=',', quotechar='\"')
+        if(len(cols)==4):
+            if(len(cols[0])==0):#if first thing is empty
+                csvWriter.writerow([minister]+cols[1:]+[year]+[inputfile])
+            else:
+                csvWriter.writerow(cols+[year]+[inputfile])
+        else:
+            if(len(cols[0])==0):#if first thing is empty
+                csvWriter.writerow([minister]+cols[1:-1]+[year]+[inputfile])
+            else:
+                csvWriter.writerow(cols[:-1]+[year]+[inputfile])
+
+
+
+    # writeMinisters(minister)
     # print(line, minister)
-    orgs = line[2].split(",")
+    # orgs = line[2].split(",")
     # print(orgs)
-    writeOrgs(orgs)
+    # writeOrgs(orgs)
     #TODO:  with open(myFile, 'a') as file:
         #if first thing is empty add person
         # file.write((',').join(line))
+
 def writeMinisters(minister):
     with open("ministers.csv","a") as f:
         # print(minister)
