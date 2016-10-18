575ad771b9121a54ee6a3d91676457b47e2d94bd
Jo Osborne <jo.osborne@footballradar.com>
2016-10-18 22:57:41 +0100

    remove whitespace and handle boilerplate better

--- src/python/datapreprocessor/datapreprocessor/csvcleanser.py
+++ src/python/datapreprocessor/datapreprocessor/csvcleanser.py
@@ -17,32 +17,34 @@ def remove_empty_columns(lines):
     return data
 
 
-def remove_line(line, lines):
-    if (lines.count(line)) > 0:
-        lines.remove(line)
+def remove_whitespace(lines):
+    for line_idx, line in enumerate(lines):
+        for field_idx, field in enumerate(line):
+            lines[line_idx][field_idx] = lines[line_idx][field_idx].strip()
     return lines
 
 
+def remove_special_chars(lines):
+    for line_idx, line in enumerate(lines):
+        for field_idx, field in enumerate(line):
+            lines[line_idx][field_idx] = lines[line_idx][field_idx].replace('\xb2', '').replace('\xb1', '')
+    return lines
+
+
+def remove_line(predicate, lines):
+    return [l for l in lines if not predicate(l)]
+
 def remove_boilerplate(lines):
-    header_line = ['Minister', 'Date', 'Name of External Organisation', 'Purpose of meeting']
-    header_line_2 = ['Minister', 'Date', 'Name of Organisation', 'Purpose of meeting']
-
-    footer_line_1 = ['Does not normally include meetings with Government bodies such as other Government Departments, NDPBs, Non-Ministerial Departments, Agencies, Government reviews and representatives of devolved or foreign governments', '', '', '']
-    footer_line_2 = ['Note', '', '', '']
-    footer_line_3 = ['Does not normally include meetings with Government bodies such as other Government departments, NDPBs, Non-Ministerial Departments, Agencies, Government reviews and representatives of Parliament, devolved or foreign governments.', '', '', '']
-    footer_line_4 = ['Does not normally include meetings with Government bodies such as other Government Departments, NDPBs, Non-Ministerial Departments, Agencies, Government reviews and representatives of devolved or foreign governments.', '', '', '']
-
-    lines = remove_line(header_line, lines)
-    lines = remove_line(header_line_2, lines)
-    lines = remove_line(footer_line_1, lines)
-    lines = remove_line(footer_line_2, lines)
-    lines = remove_line(footer_line_3, lines)
-    lines = remove_line(footer_line_4, lines)
-    
+    lines = remove_line(lambda l: l == ['Minister', 'Date', 'Name of Organisation', 'Purpose of meeting'], lines)
+    lines = remove_line(lambda l: l == ['Minister', 'Date', 'Name of External Organisation', 'Purpose of meeting'], lines)
+    lines = remove_line(lambda l: 'Does not normally include' in l[0], lines)
+    lines = remove_line(lambda l: l[0] == 'Note', lines)
     return lines
 
 def cleanse_csv_data(file_contents):
     file_contents = remove_empty_lines(file_contents)
     file_contents = remove_empty_columns(file_contents)
+    file_contents = remove_whitespace(file_contents)
+    file_contents = remove_special_chars(file_contents)
     file_contents = remove_boilerplate(file_contents)
     return file_contents
