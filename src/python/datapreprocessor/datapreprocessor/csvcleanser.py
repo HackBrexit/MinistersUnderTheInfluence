18d8f09795de8e7315f4f5fc63f0e7caf23bfaa4
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2016-11-16 23:18:12 +0000

    Refactor cleanse_csv_data
    Issue #81
    
    Refactor cleansing of cvs data. Add a function to cleanse a single row
    (or return None if the row isn’t useful), then run this over all rows
    and filter out any we’re not interested in.
    
    Remove the functions that operate over the entire set of rows as
    they’re no longer needed.

--- src/python/datapreprocessor/datapreprocessor/csvcleanser.py
+++ src/python/datapreprocessor/datapreprocessor/csvcleanser.py
@@ -7,36 +7,16 @@ def is_row_empty(row):
     return not any(row)
 
 
-def remove_empty_lines(rows):
-    return [row for row in rows if not is_row_empty(row)]
-
-
 def remove_excess_columns_from_row(row):
     return row[:4]
-
-
-def remove_empty_columns(rows):
-    return [remove_excess_columns_from_row(row) for row in rows]
 
 
 def remove_extra_whitespace_from_row(row):
     return [cell.strip() for cell in row]
 
 
-def remove_whitespace(rows):
-    return [remove_extra_whitespace_from_row(row) for row in rows]
-
-
 def remove_special_chars_from_row(row):
     return [cell.translate(None, UNWANTED_SPECIAL_CHARS) for cell in row]
-
-
-def remove_special_chars(rows):
-    return [remove_special_chars_from_row(row) for row in rows]
-
-
-def remove_line(predicate, lines):
-    return [l for l in lines if not predicate(l)]
 
 
 def is_row_boilerplate(row):
@@ -47,14 +27,15 @@ def is_row_boilerplate(row):
     )
 
 
-def remove_boilerplate(rows):
-    return remove_line(is_row_boilerplate, rows)
+def cleanse_row(row):
+    row_data = remove_excess_columns_from_row(row)
+    row_data = remove_special_chars_from_row(row_data)
+    row_data = remove_extra_whitespace_from_row(row_data)
+
+    if is_row_empty(row_data) or is_row_boilerplate(row_data):
+        return None
+    return row_data
 
 
 def cleanse_csv_data(file_contents):
-    file_contents = remove_empty_lines(file_contents)
-    file_contents = remove_empty_columns(file_contents)
-    file_contents = remove_whitespace(file_contents)
-    file_contents = remove_special_chars(file_contents)
-    file_contents = remove_boilerplate(file_contents)
-    return file_contents
+    return filter(None, (cleanse_row(row) for row in file_contents))
