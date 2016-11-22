99fb7ccec5b4856949ee130b025d6063c6f910bb
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2016-11-22 21:55:33 +0000

    Annotate the script
    Issue #86

--- src/python/org_fetcher/fetch_orgs.py
+++ src/python/org_fetcher/fetch_orgs.py
@@ -6,38 +6,55 @@ PATH_TO_DATAFILES = os.path.join(
     '..', '..', 'php', 'gov-harvester', 'datafiles'
 )
 
+
 orgs = defaultdict(lambda: 0)
 for filename in os.listdir(PATH_TO_DATAFILES):
     filename_lower = filename.lower()
     if not filename_lower.endswith('.csv'):
+        # Skip the file if it's not a csv
         continue
     if not 'meetings' in filename_lower:
+        # Skip the file if it doesn't contain a meeting of some sort
         continue
     if 'gift' in filename_lower:
+        # Skip the file if it also contains gift information
         continue
     with open(os.path.join(PATH_TO_DATAFILES, filename), 'rU') as file_handle:
         reader = csv.reader(file_handle)
         first_row = True
         org_col = None
         for row in reader:
-            if len(row) < 4:
+            if len(row) < 3:
+                # If there's fewer than three columns this row is unlikely to be useful
+                # Mostly we'd want 4 but sometimes it's meetings for XXX followed by a
+                # three column version with the other details.
                 continue
             if first_row and not row[1]:
+                # If the first row isn't full of headings then skip it and treat the
+                # next as if it's a heading
                 continue
             if first_row:
+                # When looking at the heading row, scan across to find the column with
+                # the organisations in.
                 first_row = False
                 for index, col in enumerate(row):
                     if 'Health' in col:
+                        # Special case, there's one file where this is in the minister
+                        # heading too.
                         continue
                     if 'organisation' in col.lower() or 'organization' in col.lower():
                         org_col = index
                         break
                 if org_col is None:
+                    # If there wasn't an 'organisation' column then skip to the next file
                     break
                 continue
             if org_col < len(row) and row[org_col]:
+                # If the current row has enough columns extract the organisation
+                # and clean it up
                 org = row[org_col].strip().strip('-').strip()
                 if org:
+                    # Increment the organisation's meeting count
                     orgs[org] += 1
 
 with open('orgs.csv', 'wb') as org_file:
