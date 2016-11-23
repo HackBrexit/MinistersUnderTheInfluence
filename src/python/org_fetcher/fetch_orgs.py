e80c1670d994b4f14bc02c6c34eeabbb7c8e81c7
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2016-11-23 22:31:28 +0000

    Refactor
    No need to track if it’s a header row, if we’ve not found an
    organisation column then we’re still looking for a header row.
    Extract cleaning function

--- src/python/org_fetcher/fetch_orgs.py
+++ src/python/org_fetcher/fetch_orgs.py
@@ -31,22 +31,27 @@ def file_can_be_processed(filename):
     return all(check(filename_lower) for check in CAN_PROCESS_FILE_CHECKS)
 
 
-def row_is_long_enough(row):
+def row_is_long_enough(row, must_contain_index=2):
     """
     Check we have enough entries to be a data or header row.
 
     If there's fewer than three columns this row is unlikely to be useful
+    therefore by default row[2] must exist.
     Mostly we'd want 4 but sometimes it's meetings for XXX followed by a
     three column version with the other details.
     """
-    return len(row) >= 3
+    return len(row) > must_contain_index
 
 
 def header_row_contains_more_than_one_value(row):
     """
     Check that at least the first two columns of a header have values
     """
-    return is_header_row and row[1]
+    return bool(row[1])
+
+
+def organisation_column_contains_a_value(row, organisation_column_index):
+    return bool(row[organisation_column_index])
 
 
 CAN_PROCESS_HEADER_ROW_CHECKS = [
@@ -57,14 +62,18 @@ CAN_PROCESS_HEADER_ROW_CHECKS = [
 
 CAN_PROCESS_DATA_ROW_CHECKS = [
     row_is_long_enough,
+    organisation_column_contains_a_value,
 ]
 
 
-def can_process_row(row, is_header_row):
-    if is_header_row:
+def can_process_row(row, organisation_column_index):
+    if organisation_column_index is None:
         return all(check(row) for check in CAN_PROCESS_HEADER_ROW_CHECKS)
     else:
-        return all(check(row) for check in CAN_PROCESS_DATA_ROW_CHECKS)
+        return all(
+            check(row, organisation_column_index)
+            for check in CAN_PROCESS_DATA_ROW_CHECKS
+        )
 
 
 def find_organisation_column_index(row):
@@ -78,28 +87,28 @@ def find_organisation_column_index(row):
     return None
 
 
+def clean_organisation_string(organisation_string):
+    # Remove leading/trailing whitespace, dashes and more whitespace
+    return organisation_string.strip().strip('-').strip()
+
+
 orgs = defaultdict(lambda: 0)
 for filename in os.listdir(PATH_TO_DATAFILES):
     if not file_can_be_processed(filename):
         continue
     with open(os.path.join(PATH_TO_DATAFILES, filename), 'rU') as file_handle:
         reader = csv.reader(file_handle)
-        is_header_row = True
         org_col = None
         for row in reader:
-            if not can_process_row(row, is_header_row):
+            if not can_process_row(row, org_col):
                 continue
-            if is_header_row:
+            if org_col is None:
                 org_col = find_organisation_column_index(row)
                 if org_col is None:
                     # If there wasn't an 'organisation' column then skip to the next file
                     break
-                is_header_row = False
-                continue
-            if org_col < len(row) and row[org_col]:
-                # If the current row has enough columns extract the organisation
-                # and clean it up
-                org = row[org_col].strip().strip('-').strip()
+            else:
+                org = clean_organisation_string(row[org_col])
                 if org:
                     # Increment the organisation's meeting count
                     orgs[org] += 1
