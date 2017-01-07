4fb86f837c55911c15cca924d85196aa38c8a8ae
Adam Spiers <hackbrexit@adamspiers.org>
2017-01-07 23:28:52 +0000

    UI: use rack-cors for Cross-Origin Resource Sharing
    The UI can't talk to the backend without Cross-Origin Resource Sharing:
    
    https://www.nczonline.net/blog/2010/05/25/cross-domain-ajax-with-cross-origin-resource-sharing/
    https://github.com/cyu/rack-cors

--- src/ruby/meetings/config/application.rb
+++ src/ruby/meetings/config/application.rb
@@ -32,5 +32,12 @@ module Meetings
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
