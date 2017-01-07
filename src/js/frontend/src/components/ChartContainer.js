8ae8c0fae85c5c52c867789cc60f39eca075417d
Adam Spiers <hackbrexit@adamspiers.org>
2017-01-07 12:59:51 +0000

    UI: make fake data a bit more realistic
    This gives us something a bit nicer to test with.

--- src/js/frontend/src/components/ChartContainer.js
+++ src/js/frontend/src/components/ChartContainer.js
@@ -9,8 +9,12 @@ class ChartContainer extends React.Component{
     super(props);
     this.state = {
       data: [
-          {entity: "Lobbyists-are-us", meetingCount: 10},
-          {entity: "Defenders of freendome", meetingCount: 2}
+          {category: "Health", meetingCount: 10},
+          {category: "Finance", meetingCount: 20},
+          {category: "Arms", meetingCount: 14},
+          {category: "IT", meetingCount: 1},
+          {category: "Charitable Sector", meetingCount: 8},
+          {category: "Media", meetingCount: 5}
       ]
     }
   }
