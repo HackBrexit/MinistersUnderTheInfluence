ca0c670e593044ed17ad2c41afc0133fd623cd80
John Small <john.small@bbc.com>
2017-01-20 07:27:44 +0000

    try moving the .ruby-version and .ruby-gemset files to the project root to see if Capistrano likes it better

--- src/ruby/meetings/config/deploy.rb
+++ src/ruby/meetings/config/deploy.rb
@@ -13,6 +13,7 @@ set :apache_domain,->{fetch(:domain)}
 # Default value for :scm is :git
 # set :scm, :git
 set :git_strategy, Capistrano::Git::SubmoduleStrategy
+set :repo_tree, 'src/ruby/meetings'
 # Default value for :format is :pretty
 # set :format, :pretty
 
