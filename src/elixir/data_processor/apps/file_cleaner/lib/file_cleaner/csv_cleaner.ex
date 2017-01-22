379c92eb44dd9f730d1106b1b26945d122100a49
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-22 22:42:08 +0000

    Run through each row and try to 'clean' it

--- src/elixir/data_processor/apps/file_cleaner/lib/file_cleaner/csv_cleaner.ex
+++ src/elixir/data_processor/apps/file_cleaner/lib/file_cleaner/csv_cleaner.ex
@@ -1,12 +1,39 @@
 defmodule FileCleaner.CSVCleaner do
   NimbleCSV.define(CSVParser, separator: ",", escape: "\"")
 
+  defmodule RowState do
+    defstruct previous_minister: :nil
+  end
+
+  defmodule MeetingRow do
+    defstruct minister: :nil, start_date: :nil, end_date: :nil, organisations: :nil, reason: "", row: 0
+  end
+
   def clean_file(file_metadata=%{data_type: :meetings}) do
+    file_metadata.filename
+    |> File.stream!
+    |> CSVParser.parse_stream(headers: :false)
+    |> Stream.with_index
+    |> Stream.transform(:header, &(clean_meeting_row &1, &2, file_metadata))
+    |> Stream.reject(&(&1 == :header))
+    |> Enum.to_list |> inspect |> IO.puts
+
     IO.puts inspect(file_metadata)
     {:ok, file_metadata}
   end
 
   def clean_file(file_metadata) do
     {:error, :unsupported_data_type, file_metadata}
+  end
+
+  defp clean_meeting_row(row, :header, file_metadata) do
+    IO.puts inspect(row)
+    # Can make a call here to validate the headers are sensible
+    {[:header], %RowState{}}
+  end
+
+  defp clean_meeting_row(row, row_state, file_metadata) do
+    new_row = %MeetingRow{}
+    {[new_row], Map.put(row_state, :previous_minister, new_row.minister)}
   end
 end
