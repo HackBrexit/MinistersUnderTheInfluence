0e8a534d44b950f6cfa77ae32c72240ff7f3ff4e
John Small <john.small@bbc.com>
2017-01-22 20:46:39 +0000

    set production deploy directory

--- src/ruby/meetings/config/deploy/production.rb
+++ src/ruby/meetings/config/deploy/production.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 set :stage, :production 
-
+set :deploy_to, '/home/rails/muti_production'
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server definition into the
