58e43d0ee3390144662645a37090a073b6bfde81
Adam Spiers <hackbrexit@adamspiers.org>
2017-01-24 12:16:34 +0000

    ui: fix aboutReducer
    It had three problems:
    
      - The default state was an Object (the entire Redux state)
        not a boolean (the value of the showAboutScreen key)
    
      - It was trying to operate on the whole Redux state, not just
        the value of the showAboutScreen key.
    
      - Consequently it changed state even if there was no TOGGLE_ABOUT
        action, although this accidentally ended up appearing to work for
        some strange reason.

--- src/js/frontend/src/Redux/reducer.js
+++ src/js/frontend/src/Redux/reducer.js
@@ -6,7 +6,8 @@ import * as actionTypes from "./action-types";
 
 export function rootReducer (state = IMap(), action) {
   return state.merge({
-    showAboutScreen: aboutReducer(state.get("showAboutScreen", undefined), action),
+    showAboutScreen: aboutReducer(state.get("showAboutScreen", undefined),
+                                  action),
   })
   return state;
 }
@@ -22,14 +23,11 @@ const defaultState = {
   showAboutScreen: false,
 };
 
-export function aboutReducer(state = defaultState, {type}) {
-  let newState = Object.assign({}, state);
-
+export function aboutReducer(state = defaultState.showAboutScreen, {type}) {
   switch (type) {
     case actionTypes.TOGGLE_ABOUT:
-      newState.showAboutScreen = !newState.showAboutScreen;
-      return newState;
+      return !state;
     default:
-      return newState;
+      return state;
   }
 }
