2a62119e4e715879f5dbc4c7470d71887fac3cf0
Adam Spiers <hackbrexit@adamspiers.org>
2017-01-24 12:18:39 +0000

    ui: improve comment about redux action creators

--- src/js/frontend/src/Redux/actions.js
+++ src/js/frontend/src/Redux/actions.js
@@ -1,6 +1,8 @@
 import * as actionTypes from "./action-types"
 
-// example of a regular action
+// These are action creators
+// http://redux.js.org/docs/basics/Actions.html#action-creators
+
 export const toggleAbout = () => {
   return {
     type: actionTypes.TOGGLE_ABOUT,
