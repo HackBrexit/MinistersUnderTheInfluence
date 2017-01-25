500cbe64c131c3fbc59036c02620ffdf58539c0d
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-25 20:45:29 +0000

    Move supervisor into data_sanitiser
    No need for it to be in file_cleaner any more.

--- src/elixir/data_sanitiser/mix.exs
+++ src/elixir/data_sanitiser/mix.exs
@@ -19,7 +19,7 @@ defmodule DataSanitiser.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-     mod: {DataSanitiser.FileCleaner, []}]
+     mod: {DataSanitiser, []}]
   end
 
   # Dependencies can be Hex packages:
