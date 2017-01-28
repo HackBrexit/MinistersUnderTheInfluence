503ef371674f21810f6acc422af2edd733863a00
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-28 18:20:13 +0000

    Some organisations are separated by new lines!
    So add the new separator and filter out any that are empty

--- src/elixir/data_sanitiser/lib/data_sanitiser/organisation_utils.ex
+++ src/elixir/data_sanitiser/lib/data_sanitiser/organisation_utils.ex
@@ -1,10 +1,11 @@
 defmodule DataSanitiser.OrganisationUtils do
-  @separator_regex ~R{,|;| and | & |/|\\}
+  @separator_regex ~R{,|;| and | & |/|\\|\n}
 
 
   def parse_organisations(organisations_string) do
     organisations_string
     |> String.split(@separator_regex)
     |> Enum.map(&String.trim/1)
+    |> Enum.reject(&(&1 == ""))
   end
 end
