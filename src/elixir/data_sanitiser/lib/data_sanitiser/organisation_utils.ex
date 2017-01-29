de54af73dcc0e2dabe0f62258c073acadd32f962
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-29 17:56:01 +0000

    Document and test OrganisationUtils

--- src/elixir/data_sanitiser/lib/data_sanitiser/organisation_utils.ex
+++ src/elixir/data_sanitiser/lib/data_sanitiser/organisation_utils.ex
@@ -1,10 +1,41 @@
 defmodule DataSanitiser.OrganisationUtils do
+  @moduledoc """
+  Functions for dealing with organisations.
+  """
+
   alias DataSanitiser.Canonicaliser
   import DataSanitiser.GeneralUtils, only: [trim_spaces_and_commas: 1]
 
-  @separator_regex ~R{,|;| and | & |/|\\|\n}
+  @separators [
+    ",",
+    ";",
+    " and ",
+    " & ",
+    "/",
+    "\\\\", # Actually just a single \ but needs to be double escaped
+    "\\n"
+  ]
+  @separator_regex ~r{#{Enum.join @separators, "|"}}
 
 
+  @doc """
+  Convert a string of organisations into a list of them.
+
+  Currently does a very crude conversion by first splitting on 
+  #{Enum.join @separators, ","}
+  After that resulting strings have leading/trailing whitespace and commas
+  removed, empty strings are filtered out and the canonical versions of the
+  strings are looked up.
+
+  ## Examples
+      iex> import DataSanitiser.OrganisationUtils,
+      ...>   only: [parse_organisations: 1]
+      iex> parse_organisations("ORG1, Org 2, orG 3")
+      ["ORG1", "Org 2", "orG 3"]
+      iex> parse_organisations("ORG 3, org1, org 4")
+      ["orG 3", "ORG1", "org 4"]
+  """
+  @spec parse_organisations(String.t) :: [String.t]
   def parse_organisations(organisations_string) do
     organisations_string
     |> String.split(@separator_regex)
---
+++ src/elixir/data_sanitiser/test/organisation_utils_test.exs
@@ -0,0 +1,7 @@
+defmodule DataSanitiser.OrganisationUtilsTests do
+
+  defmodule DocTests do
+    use ExUnit.Case
+    doctest DataSanitiser.OrganisationUtils
+  end
+end
