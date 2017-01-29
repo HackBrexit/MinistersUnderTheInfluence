dfec9c80605ae549775308c1d5786d37a0acafce
Adrian Rogers <brexit@Adrian-Rogerss-MacBook-Pro.local>
2017-01-29 17:27:08 +0000

    Document and test general utils module
    Just use doctests for tests here.

--- src/elixir/data_sanitiser/lib/data_sanitiser/general_utils.ex
+++ src/elixir/data_sanitiser/lib/data_sanitiser/general_utils.ex
@@ -1,10 +1,36 @@
 defmodule DataSanitiser.GeneralUtils do
+  @moduledoc """
+  A collection of useful functions
+  """
 
+  @doc """
+  Removes any leading or trailing whitespace and commas
+
+  This is unicode compatible so it will remove non-breaking spaces
+  and other forms of whitespace too that don't fit within ASCII.
+
+  ## Examples
+      iex> string = "  ,,  Important, here!  \\n"
+      iex> DataSanitiser.GeneralUtils.trim_spaces_and_commas(string)
+      "Important, here!"
+      iex> DataSanitiser.GeneralUtils.trim_spaces_and_commas("All Good")
+      "All Good"
+  """
   def trim_spaces_and_commas(string) do
-    # Remove all whitespace and commas from the start and end
     Regex.replace(~r{(^[\s,]*)|([\s,]*$)}u, string,  "")
   end
 
 
+  @doc """
+  Reordered version of `Map.put` to assist with chaining
+
+  Allows you to create a value with a chain of functions and end by
+  inserting that value into a map.
+
+  ## Example
+      iex> import DataSanitiser.GeneralUtils, only: [put_into_map_at: 3]
+      iex> "A VALUE" |> String.downcase |> put_into_map_at(%{}, :lower)
+      %{lower: "a value"}
+  """
   def put_into_map_at(value, map, key), do: Map.put map, key, value
 end
---
+++ src/elixir/data_sanitiser/test/general_utils_test.exs
@@ -0,0 +1,7 @@
+defmodule DataSanitiser.GeneralUtilsTests do
+
+  defmodule DocTests do
+    use ExUnit.Case
+    doctest DataSanitiser.GeneralUtils
+  end
+end
