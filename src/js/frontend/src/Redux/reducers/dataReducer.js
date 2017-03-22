HackBrexit/MinistersUnderTheInfluence

commit 22cdf4461a9cd19e88e6d68a9a03b41b437e7910

ui: switch to combineReducers
Use redux's combineReducers and keep data and UI reducers separate.

src/js/frontend/src/Redux/reducers/dataReducer.js
@@ -1,0 +1,30 @@
+import {fromJS} from "immutable";
+import {ADD_ENTITY} from "../action-types";
+
+const initialState = fromJS({
+  entities: {
+    people: {},
+    organisations: {},
+    "government-offices": {},
+  },
+});
+
+export const dataReducer = (state = initialState, action) => {
+  let {type, entityType, id, data} = action;
+
+  switch (type) {
+    case ADD_ENTITY:
+      if (!state.has(entityType)) {
+        console.error(`Received ADD_ENTITY action with invalid entity type ${entityType}`);
+        return state;
+      }
+      return state.mergeDeep({
+        [entityType]: {
+          [id]: data
+        },
+      });
+    default:
+      return state;
+
+  }
+}

src/js/frontend/src/Redux/reducer.js
@@ -1,60 +1,9 @@
-import {Map as IMap, List as IList} from "immutable";
+import {combineReducers} from "redux";
 import * as actionTypes from "./action-types";
+import {uiReducer} from "./reducers/uiReducer";
+import {dataReducer} from "./reducers/dataReducer";
 
-////////////////////////////////////////////////////////////////////////////////
-// root reducer
-
-// FIXME: switch to use combineReducers?
-// http://redux.js.org/docs/api/combineReducers.html
-
-export function rootReducer (state = IMap(), action) {
-  return state.merge({
-    showAboutScreen: aboutReducer(state.get("showAboutScreen", undefined),
-                                  action),
-    entities: addEntityReducer(state.get("entities"), action),
-  })
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// accountsy stuff
-
-// const defaultState = IMap({
-//   showAboutScreen: false,
-// });
-
-const defaultState = {
-  showAboutScreen: false,
-  entities: {
-    people: {},
-    organisations: {},
-    "government-offices": {},
-  },
-};
-
-function aboutReducer(state = defaultState.showAboutScreen, {type}) {
-  switch (type) {
-    case actionTypes.TOGGLE_ABOUT:
-      return !state;
-    default:
-      return state;
-  }
-}
-
-function addEntityReducer(state = defaultState.entities, action) {
-  let {type, entityType, id, data} = action;
-
-  switch (type) {
-    case actionTypes.ADD_ENTITY:
-      if (!state.has(entityType)) {
-        console.error(`Received ADD_ENTITY action with invalid entity type ${entityType}`);
-        return state;
-      }
-      return state.mergeDeep({
-        [entityType]: {
-          [id]: data
-        },
-      });
-    default:
-      return state;
-  }
-}
+export const rootReducer = combineReducers({
+  ui: uiReducer,
+  data: dataReducer,
+});

src/js/frontend/src/Redux/reducers/uiReducer.js
@@ -1,0 +1,15 @@
+import {fromJS} from "immutable";
+import {TOGGLE_ABOUT} from "../action-types";
+
+const initialState = fromJS({
+    showAboutScreen: false
+});
+
+export const uiReducer = (state = initialState, {type}) => {
+  switch (type) {
+    case TOGGLE_ABOUT:
+      return state.set('showAboutScreen', !state.get('showAboutScreen'));
+    default:
+      return state;
+  }
+}

src/js/frontend/src/containers/ChartContainer.js
@@ -49,9 +49,9 @@ class ChartContainer extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({ data }) => {
   return {
-    entities: state.get("entities"),
+    entities: data.get('entities'),
   };
 };
 

commit 345b74468e84726203e311640932b913a7b08f74

ui: fix bugs in dataReducer
These were introduced by 22cdf4461a9cd19e88e6d68a9a03b41b437e7910
in #159.

src/js/frontend/src/Redux/reducers/dataReducer.js
@@ -14,17 +14,19 @@ export const dataReducer = (state = initialState, action) => {
 
   switch (type) {
     case ADD_ENTITY:
-      if (!state.has(entityType)) {
+      if (!state.get("entities").has(entityType)) {
         console.error(`Received ADD_ENTITY action with invalid entity type ${entityType}`);
         return state;
       }
       return state.mergeDeep({
-        [entityType]: {
-          [id]: data
+        entities: {
+          [entityType]: {
+            [id]: data
+          },
         },
       });
+
     default:
       return state;
-
   }
 }