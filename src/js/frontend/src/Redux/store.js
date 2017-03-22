fadea4a6aad60ab8739a17371fa0cd5e14e24fad
Adam Spiers <hackbrexit@adamspiers.org>
2017-03-22 01:22:53 +0000

    ui: include thunk middleware using redux-devtools-extension
    To correctly support both thunk middleware and Redux Devtools, we use
    the convenient redux-devtools-extension library to compose thunk with
    the middleware enhancer, and then use that as the final argument to
    createStore().

--- src/js/frontend/src/Redux/store.js
+++ src/js/frontend/src/Redux/store.js
@@ -10,6 +10,7 @@ import merger from 'redux-storage-merger-immutablejs'
 
 ////////////////////////////////////////////////////////////////////////////////
 // redux stuff
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 // wrap our main reducer in a storage reducer - this intercepts LOAD actions and
 // calls the merger function to merge in the new state
@@ -31,18 +32,21 @@ const loggerMiddleware = createLogger({
   //predicate: (getState, action) => action.type !== CALCULATION_NEEDS_REFRESH
 })
 
-// now create our redux store, applying all our middleware
-// const store = createStore(reducer, applyMiddleware(
-//   thunkMiddleware, // first, so function results get transformed
-//   loggerMiddleware, // now log everything at this state
-//   storageMiddleware // finally the storage middleware
-// ))
-
+const composeEnhancers = composeWithDevTools({
+  name: 'MUTI frontend',
+  // actionsBlacklist: ['REDUX_STORAGE_SAVE']
+});
 
 const store = createStore(
   reducer,
   initialStoreState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware, // first, so function results get transformed
+//      loggerMiddleware,  // now log everything at this state
+//      storageMiddleware, // finally the storage middleware
+    )
+  )
 );
 
 window.store = store;
